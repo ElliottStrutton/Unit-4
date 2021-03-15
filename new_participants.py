a326fd6ec05adf033d309370b7cc4be6a1dd98a4
ElliottStrutton <autoecns@gmail.com>
2021-03-15 11:27:34 +0000

    Split functions into different files

--- new_participants.py
+++ new_participants.py
@@ -1,35 +1,17 @@
-# Torment Tracker by Elliott Strutton
-
-
-ver = "1.1.0"
-
-teamOne = {}
-teamTwo = {}
-teamThree = {}
-TeamFour = {}
-Solos = {}
-
-eventOneParticipants = {}
-eventTwoParticipants = {}
-eventThreeParticipants = {}
-eventFourParticipants = {}
-eventFiveParticipants = {}
-
-
 def main():
-    # Selection menu
     print("""
 **************************************************
-1) Edit teams and participants list
-2) Edit tournament events
-3) Edit scores
-4) Calculate scores
-5) Exit
+1) Edit teams
+2) Add participants to team
+3) Remove participants from team
+4) Add solo participants
+5) Remove solo participants
+6) Back to main menu
 **************************************************    
 """)
     selection = input(">")
     if selection == "1":
-        new_participants()
+        print("1")
     elif selection == "2":
         print("2")
     elif selection == "3":
@@ -38,15 +20,8 @@ def main():
         print("4")
     elif selection == "5":
         print("5")
+    elif selection == "6":
+        print("6")
     else:
         # Help Message
         print("\n \n \nPlease enter a number that corresponds with the menu options")
-        main()
-
-
-
-
-
-print("Tournament Tracker")
-print("Version :", ver)
-main()
